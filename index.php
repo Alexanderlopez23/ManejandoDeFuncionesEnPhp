5996da3f81ca1d15496ed106677f0bf35d2fd2cb
Alex <jalopez9722@misena.edu.co>
2020-12-31 11:40:14 -0500

    Se aprende a utilizar una parte de la herramienta FileZilla que utiliza Protocolo de transferencia de archivos para el caso de entrar o dejar archivos alojados en el  servidor de byehost.

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
+<!-- http://localhost/ManejandoDeFuncionesEnPhp/index.php -->
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="es">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8"/>
+        <title> Manejo de funciones en PHP</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+    <section>
+	    <form action="controlador.php" method="POST"> 
+	    	
+	    	
+
+
+	    </form>	
+    </section>
     </body>
 </html>
